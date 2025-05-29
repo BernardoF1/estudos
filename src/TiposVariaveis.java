59a71d81e6906cb7d4c5b3aa014a28c3a40ed56a
BernardoF1 <bernardo21bf.z@gmail.com>
2025-05-29 10:45:06 -0300

    TiposVariaveis

---
+++ src/TiposVariaveis.java
@@ -0,0 +1,21 @@
+public class TiposVariaveis {
+    public static void main(String[] args) {
+        //tipos primitivos
+        // estudem a classe String que representa texto na aplicação
+
+
+        double salaryMin = 2500;
+
+        short numberShort = 1;
+        int numbernormal = numberShort;
+        short nummberShort2 = (short) numbernormal;
+
+        int number = 5;
+        number = 10;
+
+        final double VALUE_OF_PI = 3.14;
+
+
+        System.out.println(VALUE_OF_PI);
+    }
+}
